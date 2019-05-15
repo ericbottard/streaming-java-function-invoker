9218273bb370716d713cc2b5ba1d3a3996b8265f
Florent Biville <fbiville@pivotal.io>
2019-05-15 16:01:50 +0200

    Clean imports/dead code + exclude IsolatedFunction

--- src/main/java/io/projectriff/invoker/server/JavaFunctionInvoker.java
+++ src/main/java/io/projectriff/invoker/server/JavaFunctionInvoker.java
@@ -1,37 +1,20 @@
 package io.projectriff.invoker.server;
 
-import java.io.IOException;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.context.FunctionRegistry;
 import org.springframework.cloud.function.context.catalog.FunctionInspector;
+import org.springframework.cloud.function.core.IsolatedFunction;
 import org.springframework.cloud.function.deployer.EnableFunctionDeployer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.ReflectionUtils;
-import reactor.core.publisher.Flux;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * This class sets up all the necessary infrastructure for exposing a (streaming) function over riff gRPC protocol.
@@ -63,20 +46,14 @@ public class JavaFunctionInvoker {
 
 		private Server server;
 
-		@Autowired private ApplicationContext applicationContext;
-
 		Runner(FunctionInspector fi, FunctionRegistry registry) {
 			this.fi = fi;
 			this.registry = registry;
 		}
 
 		public void run() throws Exception {
-
 			Object function = lookupFunction();
-
-
 			Method m = new FunctionalInterfaceMethodResolver().resolve(function);
-
 			ReactorServerAdapter adapter = new ReactorServerAdapter(function, m, fi);
 
 			server = ServerBuilder.forPort(8080).addService(adapter).build();
@@ -90,9 +67,12 @@ public class JavaFunctionInvoker {
 
 			Field registry = processor.getClass().getDeclaredField("names");
 			registry.setAccessible(true);
-			Map map = (Map) registry.get(processor);
+			Map<?, ?> map = (Map) registry.get(processor);
 
-			return map.keySet().iterator().next();
+			Optional<?> result = map.keySet().stream()
+					.filter(f -> !(f instanceof IsolatedFunction<?, ?>))
+					.findFirst();
+			return result.get();
 		}
 
 		public void close() {
