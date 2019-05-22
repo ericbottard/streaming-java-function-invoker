fdc859b1a35be1a919bbc6a46be0b06f5723b3ba
Florent Biville <fbiville@pivotal.io>
2019-05-22 18:50:36 +0200

    Bootstrap Netty for HTTP invocations - hello-worldesque at this point

--- src/main/java/io/projectriff/invoker/server/JavaFunctionInvoker.java
+++ src/main/java/io/projectriff/invoker/server/JavaFunctionInvoker.java
@@ -2,20 +2,37 @@ package io.projectriff.invoker.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.deployer.resource.maven.MavenProperties;
-import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
 import org.springframework.cloud.function.context.FunctionRegistry;
 import org.springframework.cloud.function.deployer.EnableFunctionDeployer;
-import org.springframework.cloud.function.deployer.FunctionDeployerConfiguration;
-import org.springframework.cloud.function.deployer.FunctionProperties;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.util.ClassUtils;
+import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This class sets up all the necessary infrastructure for exposing a (streaming) function over riff gRPC protocol.
@@ -27,65 +44,41 @@ import java.lang.reflect.Method;
  */
 @SpringBootApplication
 @EnableFunctionDeployer
-//@EnableWebMvc
-public class JavaFunctionInvoker
-        implements ApplicationContextInitializer<GenericApplicationContext> {
+public class JavaFunctionInvoker {
 
-//    @Bean
-//    public HackyFunctionResolver functionResolver(FunctionRegistry functionRegistry) {
-//        return new HackyFunctionResolver(functionRegistry);
-//    }
-
+    @Bean
+    public HackyFunctionResolver functionResolver(FunctionRegistry functionRegistry) {
+        return new HackyFunctionResolver(functionRegistry);
+    }
 
     /*
      * Exposes an object capable of running a gRPC server with the function.
-     * Startup is done in an init method to work around late initialization needs of the function deployer.
+     * Startup is done in an init methodHandle to work around late initialization needs of the function deployer.
      */
-//    @Bean(initMethod = "run", destroyMethod = "close")
-//    public Runner runner(HackyFunctionResolver resolver) {
-//        return new Runner(resolver);
-//    }
-
-    @Override
-    public void initialize(GenericApplicationContext genericApplicationContext) {
-        genericApplicationContext.registerBean(Runner.class,
-                () -> new Runner(new HackyFunctionResolver(genericApplicationContext.getBean(FunctionRegistry.class))));
-        genericApplicationContext.registerBean(FunctionDeployerConfiguration.class, FunctionDeployerConfiguration::new);
-        genericApplicationContext.registerBean(
-                "org.springframework.cloud.function.deployer.FunctionCreatorConfiguration",
-                ClassUtils.resolveClassName(
-                        "org.springframework.cloud.function.deployer.FunctionCreatorConfiguration",
-                        genericApplicationContext.getClassLoader()));
-        genericApplicationContext.registerBean(
-                MavenProperties.class, () -> genericApplicationContext
-                        .getBean(FunctionDeployerConfiguration.class).mavenProperties(),
-                def -> {
-                    def.setFactoryBeanName(FunctionDeployerConfiguration.class.getName());
-                    def.setFactoryMethodName("mavenProperties");
-                });
-        genericApplicationContext.registerBean(FunctionProperties.class, () -> genericApplicationContext
-                        .getBean(FunctionDeployerConfiguration.class).functionProperties(),
-                def -> {
-                    def.setFactoryBeanName(FunctionDeployerConfiguration.class.getName());
-                    def.setFactoryMethodName("functionProperties");
-                });
-        genericApplicationContext.registerBean(DelegatingResourceLoader.class,
-                () -> genericApplicationContext.getBean(FunctionDeployerConfiguration.class)
-                        .delegatingResourceLoader(
-                                genericApplicationContext.getBean(MavenProperties.class)));
+    @Bean(initMethod = "run", destroyMethod = "close")
+    public GrpcRunner grpcRunner(HackyFunctionResolver resolver) {
+        return new GrpcRunner(resolver);
     }
 
-    private static class Runner {
+    /*
+     * Exposes an object capable of running a gRPC server with the function.
+     * Startup is done in an init methodHandle to work around late initialization needs of the function deployer.
+     */
+    @Bean(initMethod = "run")
+    public HttpRunner httpRunner(HackyFunctionResolver resolver) {
+        return new HttpRunner(resolver);
+    }
+
+    private static class GrpcRunner {
 
         private final HackyFunctionResolver resolver;
 
         private Server server;
 
-        Runner(HackyFunctionResolver resolver) {
+        GrpcRunner(HackyFunctionResolver resolver) {
             this.resolver = resolver;
         }
 
-        @PostConstruct
         public void run() throws Exception {
             Object function = resolver.resolveFunction();
             Method m = new FunctionalInterfaceMethodResolver().resolve(function);
@@ -95,10 +88,89 @@ public class JavaFunctionInvoker
             server.start();
         }
 
-        @PreDestroy
         public void close() {
             server.shutdown();
         }
 
+    }
+
+    private static class HttpRunner {
+
+        private static final int PORT = 8080;
+        private final HackyFunctionResolver resolver;
+
+        HttpRunner(HackyFunctionResolver resolver) {
+            this.resolver = resolver;
+        }
+
+        public void run() throws Exception {
+            Object function = resolver.resolveFunction();
+            Method m = new FunctionalInterfaceMethodResolver().resolve(function);
+            MethodHandle mh = MethodHandles.publicLookup().unreflect(m);
+            mh = mh.bindTo(function);
+
+            // Configure the server.
+            EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new CustomChannelInitializer(new MethodHandler(mh)));
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            ch.closeFuture().sync();
+        }
+    }
+
+    private static class CustomChannelInitializer extends ChannelInitializer<SocketChannel> {
+        private final MethodHandler methodHandler;
+
+        public CustomChannelInitializer(JavaFunctionInvoker.MethodHandler methodHandler) {
+            this.methodHandler = methodHandler;
+        }
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            ChannelPipeline pipeline = socketChannel.pipeline();
+            pipeline.addLast(new HttpRequestDecoder());
+            pipeline.addLast(new HttpResponseEncoder());
+            pipeline.addLast(methodHandler);
+        }
+    }
+
+    private static class MethodHandler extends SimpleChannelInboundHandler<Object> {
+
+        private final MethodHandle methodHandle;
+
+        public MethodHandler(MethodHandle methodHandle) {
+            this.methodHandle = methodHandle;
+        }
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
+            if (msg instanceof HttpContent) {
+                ByteBuf content = ((HttpContent) msg).content();
+                // TODO: methodHandle.invokeWithArguments
+            }
+            if (msg instanceof HttpRequest) {
+                // TODO ((HttpRequest) msg).???
+            }
+
+            DefaultFullHttpResponse response = new DefaultFullHttpResponse(
+                    HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK,
+                    Unpooled.copiedBuffer("hello world", StandardCharsets.UTF_8));
+
+            channelHandlerContext.write(response);
+            channelHandlerContext.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
     }
 }
